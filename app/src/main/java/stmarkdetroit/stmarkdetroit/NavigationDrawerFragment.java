2861e02aaadb4a3ca41b73338d92909ffd0aa77d
mgguirguis <mgguirguis@oakland.edu>
2015-04-23 23:04:46 -0400

    Not finished

--- app/src/main/java/stmarkdetroit/stmarkdetroit/NavigationDrawerFragment.java
+++ app/src/main/java/stmarkdetroit/stmarkdetroit/NavigationDrawerFragment.java
@@ -22,6 +22,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+
 /**
  * Fragment used for managing interactions for and presentation of a navigation drawer.
  * See the <a href="https://developer.android.com/design/patterns/navigation-drawer.html#Interaction">
